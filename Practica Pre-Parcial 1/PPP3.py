2ca95ff2de69fb465f86108e9f8490a66d516c35
Alexis Rocca <110139766+alexisrocca@users.noreply.github.com>
2023-10-11 20:21:42 +0000

    Hice el ejercicio de poker :D

--- Practica Pre-Parcial 1/PPP3.py
+++ Practica Pre-Parcial 1/PPP3.py
@@ -1,3 +1,5 @@
+from random import randint
+
 """ Ejercicio 3. Escriba una función llamada elimina que tome una lista y elimine el primer y
 último elemento de la lista. La función debe devolver una nueva lista con los elementos que no
 fueron eliminados."""
@@ -47,3 +49,135 @@ intos que tiene. Se recomienda usar la función del ejercicio anterior. """
 
 def cantidadElemDist(l:list) -> int:
     return len(eliminaDuplicados(l))
+
+# print(cantidadElemDist([1,2,3,4,5,5,6,7,6,5,4,7,1]))
+
+""" Ejercicio 8. Para comprobar si una palabra está en una lista se puede utilizar el operador in,
+pero sería una búsqueda lenta ya que busca a través de las palabras según el orden en el cual
+aparecen en la lista. Si la lista almacena las palabras en orden alfabético, podemos acelerar
+las cosas con una búsqueda dicotómica (también conocida como búsqueda binaria). Esta es
+similar a lo que hacemos cuando buscamos una palabra en el diccionario. Comenzamos por
+el centro y comprobamos si la palabra que buscamos está antes o después del centro. Si está
+antes, buscamos sólo en la primera mitad, si está después, buscamos en la segunda mitad de
+la lista. Con esto podemos reducir el tiempo de búsqueda.
+Implemente la función busquedaDicotomica que toma una lista de palabras ordenadas alfabé-
+ticamente y una palabra a buscar y retorna si la palabra está en la lista o no. """
+
+def bBinaria(l: list, e: int) -> bool:
+    i = 0
+    f = len(l)
+    m = (i+f)//2
+    while (i!=f) and (e != l[m]):
+        if (e > l[m]) and (m + 1 < len(l)):
+            i = m + 1
+        else:
+            f = m
+        m = (i+f)//2
+    return l[m] == e
+
+# print(bBinaria([1,2,3,4,5,6,7,8,9],8))
+
+""" Ejercicio 9. Escriba un programa que tenga una función que tome una cadena y muestre
+cada caracter que la forma del último al inicial """
+
+def cadenaUaI(s: str) -> str:
+    s_return = ""
+    for c in range(len(s)-1,-1,-1):
+        s_return += s[c]
+    return s_return
+
+# print(cadenaUaI("Marina"))
+
+""" Ejercicio 10. Escriba un programa que contenga a la función contar(l, x) que cuente cuán-
+tas veces aparece un carácter l dado en una cadena """
+
+def contar(l: list, x: str) -> int:
+    cant_x = 0
+    for c in l:
+        if c == x:
+            cant_x += 1
+    return cant_x
+
+# print(contar("holaaa cuantas veces aparece a","a"))
+
+""" Ejercicio 11. Escriba un programa que cuente cúantas veces aparecen cada una de las vo-
+cales en una cadena. No importa si la vocal aparece en mayúscula o en minúscula """
+
+def contarVocales(s: str) -> str:
+    vocales = ["a","e","i","o","u"]
+    vocales_contadas = 0
+    for c in s.lower():
+        if (
+            (c == vocales[0]) or
+            (c == vocales[1]) or
+            (c == vocales[2]) or
+            (c == vocales[3]) or
+            (c == vocales[4])
+            ):
+            vocales_contadas += 1
+    return vocales_contadas
+
+# print(contarVocales("vocales"))
+        
+""" Ejercicio 12. Escriba un programa que contenga una función que reciba como parámetro una
+cadena de palabras separadas por espacios y devuelva como resultado cuántas palabras de
+más de cinco letras tiene la cadena dada. """
+
+def palabrasCincoLetras(s: str) -> int:
+    c_palabras = 0
+    for palabra in s.split():
+        if len(palabra) >= 5:
+            c_palabras += 1
+    return c_palabras
+
+# print(palabrasCincoLetras("Hola hola hola hola palabra"))
+
+""" Ejercicio 13. Cartas como tuplas
+a) Proponga una representación con tuplas para las cartas de la baraja francesa.
+b) Escriba una función llamada poker que reciba cinco cartas de la baraja francesa e informe
+(devuelva el valor lógico correspondiente) si esas cartas forman o no un póker (es decir
+que hay 4 cartas con el mismo número). """
+
+def nTiradasBarajaFrancesa(n: int) -> list: #(Numero, Palo)
+    tirada = []
+    for n in range(n):
+        num = randint(1,13)
+        palo = randint(1,4)
+        
+        if num == 1:
+            num = 'As'
+        elif num == 11:
+            num = 'J'
+        elif num == 12:
+            num = 'Q'
+        elif num == 13:
+            num = 'K'
+        
+        if palo == 1:
+            palo = 'Trebol'
+        if palo == 2:
+            palo = 'Diamante'
+        if palo == 3:
+            palo = 'Corazones'
+        if palo == 4:
+            palo = 'Picas'
+        
+        tirada.append((num,palo))
+        
+    # print(tirada)
+    return tirada
+
+# print(nTiradasBarajaFrancesa(10))
+
+def poker(t: list) -> bool:
+    return (
+        t[0][0] == t[1][0] and \
+        t[1][0] == t[2][0] and \
+        t[2][0] == t[3][0]
+        )
+
+for n in range(50000):
+    isPoker = poker(nTiradasBarajaFrancesa(4))
+    if isPoker:
+        print(isPoker)
+# print(poker([(6,'Corazones'),(6,'Picas'),(6,'Trebol'),(6,'Diamante')]))
